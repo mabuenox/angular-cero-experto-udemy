63a425973558187286021f13649c951e601a91ce
Miguel Ángel Bueno Ferrer <miguel.bueno@analyticalways.com>
2022-09-19 08:23:00 +0200

    Add module for counter

--- Ejercicios/angular/01-bases/src/app/app.module.ts
+++ Ejercicios/angular/01-bases/src/app/app.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
 import { HeroesModule } from './heroes/heroes.module';
+import { CounterModule } from './counter/counter.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
   ],
   imports: [
     BrowserModule,
-    HeroesModule
+    HeroesModule,
+    CounterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
