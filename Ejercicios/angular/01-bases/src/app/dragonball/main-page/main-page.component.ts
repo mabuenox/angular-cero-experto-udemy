aba55d562fc644ddea1a23504d0941a894c7bdf0
Miguel Ángel Bueno Ferrer <miguel.bueno@analyticalways.com>
2022-09-20 18:07:05 +0200

    Implement 2 way data binding in form

--- Ejercicios/angular/01-bases/src/app/dragonball/main-page/main-page.component.ts
+++ Ejercicios/angular/01-bases/src/app/dragonball/main-page/main-page.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 
+interface Character {
+  name: string;
+  power: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
+
+  character: Character = {
+    name: 'Trucks',
+    power: 12000
+  }
+
   createCharacter() {
-    console.log('hey');
+    console.log( this.character );
   }
 }
