b62a81cdd023e2aeb2f4d5bf74e4245cec13f2ef
Miguel Ángel Bueno Ferrer <miguel.bueno@analyticalways.com>
2022-09-27 19:24:00 +0200

    Create child component

---
+++ Ejercicios/angular/01-bases/src/app/dragonball/characters/characters.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-characters',
+  templateUrl: './characters.component.html'
+})
+export class CharactersComponent {
+  characters: any[] = [];
+}
--- Ejercicios/angular/01-bases/src/app/dragonball/dragonball.module.ts
+++ Ejercicios/angular/01-bases/src/app/dragonball/dragonball.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page/main-page.component';
 import { FormsModule } from '@angular/forms';
+import { CharactersComponent } from './characters/characters.component';
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    CharactersComponent
   ],
   exports: [
     MainPageComponent
