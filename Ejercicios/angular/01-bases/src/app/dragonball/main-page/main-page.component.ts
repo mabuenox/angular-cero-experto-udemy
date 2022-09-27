c6d22e07d5e833f22f6b41fd2a14d971663c7553
Miguel Ángel Bueno Ferrer <miguel.bueno@analyticalways.com>
2022-09-27 19:10:42 +0200

    Create and add character with spread operator

--- Ejercicios/angular/01-bases/src/app/dragonball/main-page/main-page.component.ts
+++ Ejercicios/angular/01-bases/src/app/dragonball/main-page/main-page.component.ts
@@ -10,13 +10,30 @@ interface Character {
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
+  characters: Character[] = [
+    {
+      name: 'Goku',
+      power: 14000,
+    },
+    {
+      name: 'Vegeta',
+      power: 7500,
+    },
+  ];
 
-  character: Character = {
-    name: 'Trucks',
-    power: 12000
-  }
+  newCharacter: Character = {
+    name: '',
+    power: 0,
+  };
 
   createCharacter() {
-    console.log( this.character );
+    if (this.newCharacter.name.trim().length === 0) return;
+
+    this.characters.push({ ...this.newCharacter });
+
+    console.log(this.characters[this.characters.length - 1]);
+
+    this.newCharacter.name = '';
+    this.newCharacter.power = 0;
   }
 }
