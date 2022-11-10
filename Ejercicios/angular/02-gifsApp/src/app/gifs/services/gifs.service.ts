43f53e04c0b688278ae48ecec85a5e8bf4a60a3c
Miguel Ángel Bueno Ferrer <miguel.bueno@analyticalways.com>
2022-11-10 21:37:26 +0100

    Use HttpClientModule to get gifs

--- Ejercicios/angular/02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ Ejercicios/angular/02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,14 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
+  private apiKey: string = '72Krgcbk5ThCbKB4cgbJVmZ1n7wVSEkW';
   private _history: string[] = [];
 
   get history(): string[] {
     return [...this._history];
   }
+
+  constructor(private httpClient: HttpClient) { }
 
   search(query: string) {
     if (query.length === 0) {
@@ -19,5 +23,9 @@ export class GifsService {
     }
     this._history.unshift(query);
     this._history = this._history.splice(0, 10);
+
+    this.httpClient.get('https://api.giphy.com/v1/gifs/search?api_key=72Krgcbk5ThCbKB4cgbJVmZ1n7wVSEkW&q=Pablo Escobar&limit=10')
+      .subscribe( (result: any) => console.log(result.data) )
+
   }
 }
