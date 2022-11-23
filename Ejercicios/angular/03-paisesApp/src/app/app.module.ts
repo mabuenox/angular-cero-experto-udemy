ce5336137c07169daa17af304aa42113b5c91f9f
Miguel Ángel Bueno Ferrer <miguel.bueno@analyticalways.com>
2022-11-23 20:47:31 +0100

    Implement routing

--- Ejercicios/angular/03-paisesApp/src/app/app.module.ts
+++ Ejercicios/angular/03-paisesApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -11,6 +12,7 @@ import { CountriesModule } from './countries/countries.module';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     SharedModule,
     CountriesModule
   ],
